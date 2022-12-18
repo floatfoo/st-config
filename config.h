eaea08f66dab5505693d0af48e9328f67bda41f3
floatfoo <floatfoo@gmail.com>
2022-12-19 01:50:35 +0300

    gruvbox theme - patch && reduced alpha 0.8 -> 0.95

--- config.h
+++ config.h
@@ -94,30 +94,30 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.95;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 
   /* 8 normal colors */
-  [0] = "#000000", /* black   */
-  [1] = "#10110f", /* red     */
-  [2] = "#232321", /* green   */
-  [3] = "#3c3c38", /* yellow  */
-  [4] = "#5b5c56", /* blue    */
-  [5] = "#82837b", /* magenta */
-  [6] = "#afb0a5", /* cyan    */
-  [7] = "#e2e4d6", /* white   */
+  [0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+  [1] = "#cc241d", /* red     */
+  [2] = "#98971a", /* green   */
+  [3] = "#d79921", /* yellow  */
+  [4] = "#458588", /* blue    */
+  [5] = "#b16286", /* magenta */
+  [6] = "#689d6a", /* cyan    */
+  [7] = "#a89984", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#afa9d3", /* black   */
-  [9]  = "#dd7ab8", /* red     */
-  [10] = "#9da268", /* green   */
-  [11] = "#e3a58b", /* yellow  */
-  [12] = "#76bece", /* blue    */
-  [13] = "#79cca6", /* magenta */
-  [14] = "#e8b29e", /* cyan    */
-  [15] = "#6b959e", /* white   */
+ [8]  = "#928374", /* black   */
+ [9]  = "#fb4934", /* red     */
+ [10] = "#b8bb26", /* green   */
+ [11] = "#fabd2f", /* yellow  */
+ [12] = "#83a598", /* blue    */
+ [13] = "#d3869b", /* magenta */
+ [14] = "#8ec07c", /* cyan    */
+ [15] = "#ebdbb2", /* white   */
 
   /* special colors */
   [256] = "#000000", /* background */
@@ -128,9 +128,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor
  */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-unsigned int defaultcs = 257;
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 0;
+unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
 
 /*
